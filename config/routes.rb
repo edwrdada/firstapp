98b36eb3e683ad4943f628e92615275c5336d7a0
Nao Ada <0911adachi@gmail.com>
2019-03-02 03:11:58 +0900

    route changes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get "index"
-  get "show"
-  get "edit"
-  get "new"
-  get "login"
-  post "create"
-  patch "update"
-  root "index"
+  resources :users
 
 end
